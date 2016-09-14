ad69a7cc4961a609ca3b2e7c041159be162a11c2
jpoehler <jpoehler@comcast.net>
2016-09-13 19:27:58 -0500

    corrections

--- index.php
+++ index.php
@@ -11,21 +11,20 @@
      public $name;
      public $age;
 
-   public function __construct($type, $name, $age)
+   public function __construct($type, $name, $age){
       $this->type = $type;
       $this->name = $name;
-      $this->age = $age
-
+      $this->age = $age;
+}
    public function petInfo() {
-     return "Hello! My name is " . $this->name . ". I'm a " . $this->type . ", and I'm " . $this->age . " years old. Will you be my friend?"
+     return "Hello! My name is " . $this->name . ". I'm a " . $this->type . ", and I'm " . $this->age . " years old. Will you be my friend?";
    }
+}
+  $pet1 = new Pets("dog", "Woodstock", 15);
+  $pet2 = new Pets("cat", "Buster", 3);
+  $pet3 = new Pets("rabbit", "Salvadore", 2);
 
-   }
-  $pet1 = new Pet("dog", "Woodstock", 15);
-  $pet2 = new Pet("cat", "Buster", 3);
-  $pet3 = new Pet("rabbit", "Salvadore", 2);
-
-  echo $pet1->petInfo();
+  echo $pet1->petInfo(); 
   echo $pet2->petInfo();
   echo $pet3->petInfo();
    ?>
