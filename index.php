5399c7fd97db99eb17fad385e59e980054dcbc08
jpoehler <jpoehler@comcast.net>
2016-09-13 19:02:57 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<head>
+  <title>PHP Practice</title>
+  <link type = text/css rel = "stylesheet" href="stylesheet.css" />
+</head>
+<body>
+  <p>
+  <?php
+   class Pets {
+     public $type;
+     public $name;
+     public $age;
+
+   public function __construct($type, $name, $age)
+      $this->type = $type;
+      $this->name = $name;
+      $this->age = $age
+
+   public function petInfo() {
+     return "Hello! My name is " . $this->name . ". I'm a " . $this->type . ", and I'm " . $this->age . " years old. Will you be my friend?"
+   }
+
+   }
+  $pet1 = new Pet("dog", "Woodstock", 15);
+  $pet2 = new Pet("cat", "Buster", 3);
+  $pet3 = new Pet("rabbit", "Salvadore", 2);
+
+  echo $pet1->petInfo();
+  echo $pet2->petInfo();
+  echo $pet3->petInfo();
+   ?>
+     </p>
+</body>
